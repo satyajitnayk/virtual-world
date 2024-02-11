af0ca06ad13d7749ec01b6d1da3b818691b3d6f6
satyajit nayak <satyajitn15@gmail.com>
2024-02-11 21:56:50 +0530

    implement basic zoom

---
+++ js/viewport.js
@@ -0,0 +1,27 @@
+class Viewport {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');
+
+    this.zoom = 1;
+
+    this.#addEventListeners();
+  }
+
+  #addEventListeners() {
+    // handla mous wheel event
+    this.canvas.addEventListener(
+      'mousewheel',
+      this.#handleMouseWheel.bind(this)
+    );
+  }
+
+  #handleMouseWheel(evt) {
+    // gives you how much we scrolled(+ve for up & -ve for down)
+    const scrollDirection = Math.sign(evt.deltaY);
+    const step = 0.1;
+    this.zoom += scrollDirection * step;
+    // cap zoom value b/w 1 & 5
+    this.zoom = Math.max(1, Math.min(5, this.zoom));
+  }
+}
