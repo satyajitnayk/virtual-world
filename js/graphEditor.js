e9af49c6e4011a33bfdeae4787bf9151ff9b0ab6
satyajit nayak <satyajitn15@gmail.com>
2024-02-11 18:53:59 +0530

    on mouse click add a point to canvas

---
+++ js/graphEditor.js
@@ -0,0 +1,22 @@
+class GraphEditor {
+  constructor(canvas, graph) {
+    this.canvas = canvas;
+    this.graph = graph;
+
+    this.ctx = this.canvas.getContext('2d');
+
+    this.#addEventListeners();
+  }
+
+  #addEventListeners() {
+    this.canvas.addEventListener('mousedown', (evt) => {
+      // get mouse pointer location
+      const mouse = new Point(evt.offsetX, evt.offsetY);
+      this.graph.addPoint(mouse);
+    });
+  }
+
+  display() {
+    this.graph.draw(this.ctx);
+  }
+}
