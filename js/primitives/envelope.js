ae21b98ca4895ff1287ba93e7c52a1388bff03c1
satyajit nayak <satyajitn15@gmail.com>
2024-02-14 21:11:03 +0530

    update comment for readbility'

--- js/primitives/envelope.js
+++ js/primitives/envelope.js
@@ -9,13 +9,15 @@ class Envelope {
   #generatePolygon(width, roundness) {
     const { p1, p2 } = this.skeleton;
     const radius = width / 2;
-    // angle between p1 and p2 along the x-axis.
+    // angle between vector p1 and p2
     const alpha = angle(subtract(p1, p2));
     // angles in clockwise and counterclockwise directions relative to alpha.
     const alphaClockWise = alpha + Math.PI / 2;
     const alphaCounterClockWise = alpha - Math.PI / 2;
 
     const points = [];
+    // Defines a step angle (step) based on the desired roundness (more points for higher roundness)
+    // and a small tolerance (eps) for numerical stability.
     const step = Math.PI / Math.max(1, roundness);
     const eps = step / 2;
     for (let i = alphaCounterClockWise; i <= alphaClockWise + eps; i += step) {
