166269364d3bd225e111a7e11f1b44ef6cb35e1e
satyajit nayak <satyajitn15@gmail.com>
2024-02-17 16:49:04 +0530

    save and load offset & zoom info of viewport from file

--- js/world.js
+++ js/world.js
@@ -46,6 +46,8 @@ class World {
     world.trees = info.trees.map(t => new Tree(t.center, info.treeSize))
     world.laneGuides = info.laneGuides.map(g => new Segment(g.p1, g.p2))
     world.markings = info.markings.map(m => Marking.load(m))
+    world.offset = info.offset
+    world.zoom = info.zoom
     return world
   }
 
